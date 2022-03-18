b495d9d075962cf936646ed31ebe9ccc635039f0
Honorata92 <h.sajdak1@o2.pl>
2022-03-18 21:52:16 +0100

    Struktura zadania

--- Lab2/Person.cs
+++ Lab2/Person.cs
@@ -1,13 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//+ to składnik publiczny (public)
+//# to składnik chroniony (protected)
+//-to składnik prywatny(private)
+//~to składnik dostępny w obrębie projektu (package)
 
 namespace Lab2
 {
     internal class Person
     {
+        public Person(string name, int age)
+        {
+        }
+
         private string imie;
         private int age;
 
         public string Name { get; set; }
         public int Age { get; set; }
+    }
+
+    public class Student
+    { //#->private
+        private string group;
+        private List<Task> tasks = new List<Task>();
+    }
+
+    public class Task
+    {
+        public string name;
+        public TaskStatus status;
+
+        public string Name { get; set; }
+        public TaskStatus Status { get; set; }
+    }
+
+    public enum TaskStatus
+    {
+        Waiting,
+        Progress,
+        Done,
+        Rejected
     }
 }
