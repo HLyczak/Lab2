068dc44a004bce711fdb3225ed0930dc1f8f1db8
Honorata92 <h.sajdak1@o2.pl>
2022-03-18 21:20:43 +0100

    Dodaj pliki projektów.

---
+++ Lab2/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab2
+{
+    internal class Person
+    {
+        private string imie;
+        private int age;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
